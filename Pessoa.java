8b19f32b26c93ce74481e9537d1e8aebbde8b5bc
ThaynaIsabela <thaynacollaco66@gmail.com>
2020-03-13 21:49:02 -0300

    Classe Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa
+{
+    //O que ela tem? (atributos)
+    String rg;
+    String cpf;
+    String nome;
+    String endereco;
+    String email;
+    int idade;
+    boolean doador;
+    
+    //p - variavel de classe
+    //Pessoa p = new Pessoa() - endereco
+    //int x = 10 - atribui
+}
---
+++ TestePessoa.java
@@ -0,0 +1,14 @@
+public class TestePessoa
+{
+    public static void main(String[] args){
+        
+        int x = 10;
+        System.out.println(x);
+        
+        Pessoa p = new Pessoa();
+        System.out.println(p);
+        
+        Pessoa p2 = null;
+        System.out.println(p2);
+    }
+}
